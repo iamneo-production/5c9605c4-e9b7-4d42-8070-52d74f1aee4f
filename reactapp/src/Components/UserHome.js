ed9aff3d86e03395f7caadb0c4c7056e8369b999
santhosh-skct-git <18tuit124@skct.edu.in>
2022-02-22 06:01:35 +0000

    changes made

--- reactapp/src/Components/UserHome.js
+++ reactapp/src/Components/UserHome.js
@@ -1,15 +1,36 @@
-import React,{useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import UserDashboard from './UserDashboard';
-
+import axios from 'axios'
+import { Card, Row, Col, Container } from 'react-bootstrap';
 function UserHome() {
-    useEffect(() => {
-        document.title = "TravelYaari  ||  User";
-      },[]);
-
+  useEffect(() => {
+    document.title = "TravelYaari  ||  User";
+  }, []);
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+      .then(res => {
+        console.log(res)
+        setPosts(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
   return <div>
-      <UserDashboard/>
-      
+    <UserDashboard />
+    <Container>
+      <Row>
+        {
+          posts?.map((post)=>{
+            return(<Col>
+            <Card key={post.id}>{post.title}</Card>
+            </Col>)
+          })
+        }
+      </Row>
+    </Container>
   </div>
 
 
