25f3ccaf47b59d7313dd9a7620ba56dac29e2809
arpitraj5 <arpitb2.raj@gmail.com>
2022-04-06 13:32:52 +0000

    Arpit booking

--- springapp/src/main/java/com/examly/springapp/dao/UserDao1.java
+++
@@ -1,6 +0,0 @@
-package com.examly.springapp.dao;
-import com.examly.springapp.model.UserDaoo;
-import org.springframework.data.repository.CrudRepository;
-public interface UserDao1 extends CrudRepository<UserDaoo, Integer> {
-    UserDaoo findByUsername(String username);
-}
